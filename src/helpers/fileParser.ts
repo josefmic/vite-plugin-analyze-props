0759228326bd67efcd226cdd2c56ff6b62f1db23
Josef Michálek <michajo6@fel.cvut.cz>
2024-10-15 17:12:08 +0200

    fix

--- src/helpers/fileParser.ts
+++ src/helpers/fileParser.ts
@@ -1,14 +1,15 @@
-import { parse } from "@babel/parser";
+import { parse } from '@babel/parser';
 import fs from 'fs';
 
 /**
- * Parses the given file and returns the AST.
+ * Parses the given file, applies transformations, and returns the AST.
  * @param {string} filePath - The path to the file to parse.
- * @returns {import('@babel/types').File} - The parsed AST.
+ * @returns {import('@babel/types').File} - The transformed and parsed AST.
  */
 export function parseFile(filePath: string) {
     const code = fs.readFileSync(filePath, 'utf-8');
-    return parse(code, {
+
+    return parse(code ?? "", {
         sourceType: 'module',
         plugins: [
             'jsx', 
--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,6 @@ export default {
         output: {
           exports: 'named',
         },
-    },
-  }
+      },
+    }
 }; '! '
