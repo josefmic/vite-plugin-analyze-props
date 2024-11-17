cbb3c4cc83913c41083661e99eb877e370d8db5c
Josef Michálek <michajo6@fel.cvut.cz>
2024-11-17 15:46:25 +0100

    Adding file saving

--- src/analyze/analyzeProps.ts
+++ src/analyze/analyzeProps.ts
@@ -2,18 +2,29 @@ import { PluginOption, ViteDevServer } from 'vite';
 import { setBabelPlugins } from '../helpers/config';
 import { loadFiles } from '../helpers/fileLoader';
 import { getUsedProps } from './getUsedProps';
+import path from 'path';
+import fs from 'fs';
 
 interface AnalyzePropsOptions {
     babel?: {
         plugins?: string[];
     };
     patterns: string[];
+    filePath?: string;
 }
 
 export function analyzeProps(options: AnalyzePropsOptions): PluginOption {
     const matchedFiles = loadFiles(options.patterns);
     setBabelPlugins(options?.babel?.plugins || []);
     let output = getUsedProps(matchedFiles);
+
+    if (options.filePath) {
+        const resolvedPath = path.resolve(options.filePath);
+        const dirPath = path.dirname(resolvedPath);
+        
+        fs.mkdirSync(dirPath, { recursive: true });
+        fs.writeFileSync(resolvedPath, JSON.stringify(output, null, 2), 'utf-8');
+    }
 
     return {
         name: 'vite-plugin-analyze-props',
